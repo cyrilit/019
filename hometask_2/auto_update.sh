65e86175495653c133c18f9cb436ff592a33ab68
Kyrylo Anufriienko <cyrility@gmail.com>
2020-02-03 22:39:15 +0200

    Update auto_update.sh

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 
+sudo touch /var/log/update-$(date+%Y%m%d).log
+sudo apt update > /var/log/update-$(date +%Y%m%d).log
+sudo apt-get upgrade -y > /var/log/update-$(date +%Y%m%d).log
+sudo mv auto_update.sh  /etc/cron.weekly
