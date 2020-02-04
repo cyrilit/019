6f4fd23d554e3c9b1e26f8438d29ad57a5ab33a4
Kyrylo Anufriienko <cyrility@gmail.com>
2020-02-04 05:02:56 +0200

    Modify auto_update.sh

--- hometask_2/auto_update.sh
+++ hometask_2/auto_update.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo touch /var/log/update-$(date+%Y%m%d).log
-sudo apt update > /var/log/update-$(date +%Y%m%d).log
-sudo apt-get upgrade -y > /var/log/update-$(date +%Y%m%d).log
-sudo mv auto_update.sh  /etc/cron.weekly
+LOGFILE=/var/log/update-$(date +"%Y%m%d").log
+
+sudo touch $LOGFILE
+sudo chmod 666 $LOGFILE
+sudo apt update >> $LOGFILE
+sudo apt-get upgrade -y >> $LOGFILE
+sudo cp auto_update.sh  /etc/cron.weekly
