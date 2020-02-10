f0d64d9b5dbe9a39b20dee24a97b3ba83a8097c0
Kyrylo Anufriienko <cyrility@gmail.com>
2020-02-11 00:24:19 +0200

    Update installation script
    The Java Runtime Environment (JRE) is packed up and ready to go when you install Confluence using the Windows or Linux installer. You don't need to install Java yourself.

--- hometask_3/installation.sh
+++ hometask_3/installation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y openjdk-11-jdk
-sudo apt install -y mysql-server
+sudo apt-get install -y mysql-server
 
 sudo mysql -e 'use mysql;
 update user set authentication_string=PASSWORD("123456") where User="root";
@@ -16,10 +15,11 @@ CONFLUENCE=atlassian-confluence-7.3.1-x64.bin
 
 if [ ! -f "$CONFLUENCE" ]; then
     echo "File $CONFLUENCE does not exist. Try to downloading..."
-    curl -O https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin
+    #curl -O https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin
+    wget -o wget.log https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin 
 fi
 
 sudo chmod +x $CONFLUENCE
 ./$CONFLUENCE -q -varfile response.varfile
 cd /opt/atlassian/confluence7_3_1/bin
-./start-confluence.sh
+./start-confluence.sh -fg
