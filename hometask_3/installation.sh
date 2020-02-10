dfd27a20c4e0e166958d7edd8afb1f6550dbe32f
Kyrylo Anufriienko <cyrility@gmail.com>
2020-02-10 23:19:08 +0200

    Create provisioning installation script
    Create provisioning installation script for OpenJDK, MySQL, Confluence

---
+++ hometask_3/installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt install -y openjdk-11-jdk
+sudo apt install -y mysql-server
+
+sudo mysql -e 'use mysql;
+update user set authentication_string=PASSWORD("123456") where User="root";
+flush privileges;
+CREATE DATABASE CONFLUENCE;
+quit'
+/etc/init.d/mysql start
+
+cd /vagrant && pwd
+
+CONFLUENCE=atlassian-confluence-7.3.1-x64.bin
+
+if [ ! -f "$CONFLUENCE" ]; then
+    echo "File $CONFLUENCE does not exist. Try to downloading..."
+    curl -O https://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-7.3.1-x64.bin
+fi
+
+sudo chmod +x $CONFLUENCE
+./$CONFLUENCE -q -varfile response.varfile
+cd /opt/atlassian/confluence7_3_1/bin
+./start-confluence.sh
